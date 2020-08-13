51c50e87dd7f305f4f319e50bf2bb54c8ea3b400
Cristobal Lopez <crolopezp@gmail.com>
2020-08-13 13:36:51 +0200

    Add entities to be avoided

--- src/main/java/com/cristofiumod/entities/CristofiuEntity.java
+++ src/main/java/com/cristofiumod/entities/CristofiuEntity.java
@@ -7,7 +7,12 @@ import net.minecraft.entity.*;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.ai.goal.*;
+import net.minecraft.entity.monster.CreeperEntity;
+import net.minecraft.entity.monster.SpiderEntity;
+import net.minecraft.entity.monster.ZombieEntity;
 import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.entity.passive.CatEntity;
+import net.minecraft.entity.passive.OcelotEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Items;
 import net.minecraft.item.crafting.Ingredient;
@@ -30,14 +35,6 @@ public class CristofiuEntity extends AnimalEntity {
         super(type, worldIn);
     }
 
-
-    //func_233666_p_ ---> registerAttributes()
-    public static AttributeModifierMap.MutableAttribute setCustomAttributes() {
-        return MobEntity.func_233666_p_()
-                .createMutableAttribute(Attributes.MAX_HEALTH, 10.0D)
-                .createMutableAttribute(Attributes.MOVEMENT_SPEED, 0.25D);
-    }
-
     @Override
     public AgeableEntity createChild(AgeableEntity ageable) {
         return ModEntityType.CRISTOFIU.get().create(this.world);
@@ -47,15 +44,16 @@ public class CristofiuEntity extends AnimalEntity {
     protected void registerGoals() {
         super.registerGoals();
         this.eatGrassGoal = new EatGrassGoal(this);
-        this.goalSelector.addGoal(0, new SwimGoal(this));
-        this.goalSelector.addGoal(1, new PanicGoal(this, 1.25D));
-        this.goalSelector.addGoal(2, new BreedGoal(this, 1.0D));
-        this.goalSelector.addGoal(3, new TemptGoal(this, 1.1D, TEMPTATION_ITEMS, false));
-        this.goalSelector.addGoal(4, new FollowParentGoal(this, 1.1D));
-        this.goalSelector.addGoal(5, this.eatGrassGoal);
-        this.goalSelector.addGoal(6, new WaterAvoidingRandomWalkingGoal(this, 1.0D));
-        this.goalSelector.addGoal(7, new LookAtGoal(this, PlayerEntity.class, 6.0F));
-        this.goalSelector.addGoal(8, new LookRandomlyGoal(this));
+        setEntitiesToAvoid(0, 6.0F, 1.0F, 3.0F);
+        this.goalSelector.addGoal(1, new SwimGoal(this));
+        this.goalSelector.addGoal(2, new PanicGoal(this, 1.25D));
+        this.goalSelector.addGoal(3, new BreedGoal(this, 1.0D));
+        this.goalSelector.addGoal(4, new TemptGoal(this, 1.1D, TEMPTATION_ITEMS, false));
+        this.goalSelector.addGoal(5, new FollowParentGoal(this, 1.1D));
+        this.goalSelector.addGoal(6, this.eatGrassGoal);
+        this.goalSelector.addGoal(7, new WaterAvoidingRandomWalkingGoal(this, 1.0D));
+        this.goalSelector.addGoal(8, new LookAtGoal(this, PlayerEntity.class, 6.0F));
+        this.goalSelector.addGoal(9, new LookRandomlyGoal(this));
     }
 
     @Override
@@ -100,5 +98,18 @@ public class CristofiuEntity extends AnimalEntity {
         } else {
             super.handleStatusUpdate(id);
         }
+    }
+
+    //func_233666_p_ ---> registerAttributes()
+    public static AttributeModifierMap.MutableAttribute setCustomAttributes() {
+        return MobEntity.func_233666_p_()
+                .createMutableAttribute(Attributes.MAX_HEALTH, 10.0D)
+                .createMutableAttribute(Attributes.MOVEMENT_SPEED, 0.25D);
+    }
+
+    private void setEntitiesToAvoid(int priority, float distance, float farSpeed, float nearSpeed) {
+        this.goalSelector.addGoal(0, new AvoidEntityGoal<>(this, ZombieEntity.class, distance, nearSpeed, nearSpeed));
+        this.goalSelector.addGoal(0, new AvoidEntityGoal<>(this, CreeperEntity.class, distance, nearSpeed, nearSpeed));
+        this.goalSelector.addGoal(0, new AvoidEntityGoal<>(this, SpiderEntity.class, distance, farSpeed, nearSpeed));
     }
 }
